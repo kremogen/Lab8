02b2545b1598e4ab25a59e6f9dff6dbb52a47c46
Kalganov Nikita <nik.kosachev@mail.ru>
2021-09-24 20:12:53 +0300

    Main load

--- src/com/kremogen/Main.java
+++ src/com/kremogen/Main.java
@@ -1,8 +1,20 @@
 package com.kremogen;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try(FileWriter writer = new FileWriter("/Users/kremogen/Desktop/labJava/Lab8/text.txt", false)) {
+            Scanner scan = new Scanner(System.in);
+            writer.write(scan.next());
+            writer.append('\n');
+            writer.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
